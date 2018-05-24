8909a54c39122f037c37162c160687f1ac361562
Martin Fracker, Jr <martin.frackerjr@gmail.com>
2018-05-24 11:40:27 -0500

    Get development database working

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,13 @@
 
 # put any commands to run at startup for all environments here
 
+set -e
+
+>&2 echo "Migrating database"
+until dotnet ef database update; do
+    sleep 1
+done
+
+>&2 echo "DONE Migrating database"
+
 exec "$@"
