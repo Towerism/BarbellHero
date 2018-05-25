77596d42f4fbd1801b1163f481dca337995cf805
Martin Fracker <martin.frackerjr@gmail.com>
2018-05-25 10:10:17 -0500

    Run e2e tests in parallel and properly wait for website to load

--- ci/test.sh
+++ ci/test.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 
-docker build -t barbellhero_client_test ClientApp
-docker run barbellhero_client_test ng test --single-run
+if [ -z "$E2E" ]; then
+  docker build -t barbellhero_client_test ClientApp
+  docker run barbellhero_client_test ng test --single-run
+  testResult=$?
+else
+  export PORT=3000
+  docker-compose up -d
+  ci/waitForWebsite.sh
+  testResult=$?
+  if [ $testResult -eq 0 ]; then 
+    docker run --net host barbellhero_client_test ng e2e --serve false --host 0.0.0.0 --port $PORT
+  fi
+  docker-compose down
+fi
+exit $testResult
---
+++ ci/waitForWebsite.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+responseCode=""
+tries=0
+api=http://localhost:$PORT
+printf "Connecting to $api"
+while [ "$responseCode" != "200" ]; do
+  responseCode=$(curl -o /dev/null -s -w "%{http_code}" $api)
+  ((tries++))
+  printf "."
+  sleep 1
+  if [ $tries == 60 ]; then
+    printf "Failed\n"
+    >&2 echo "Could not connect to $api successfully"
+    exit 1
+  fi
+done
+printf "Connected!\n"
+exit 0
